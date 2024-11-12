83c6247ecb9b276ee58b8a10d8ce9d2edb480eb8
dlsdk0601 <dlsdk0601@gmail.com>
2024-11-12 21:13:52 +0900

    refactor: version 을 환경 변수로 셋팅 (API 버전이 올라가면 swagger 버전도 올라가야한다)

--- src/lib/swagger.ts
+++ src/lib/swagger.ts
@@ -1,11 +1,12 @@
 import { INestApplication } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { config } from "src/config";
 
 export const setupSwagger = async (app: INestApplication) => {
   const options = new DocumentBuilder()
     .setTitle("mabinogi-combocard-swagger")
     .setDescription("마비노기 콤보카드 API 스웨거")
-    .setVersion("1")
+    .setVersion(config.version)
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
