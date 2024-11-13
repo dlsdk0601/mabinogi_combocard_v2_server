fa4bc96ae9c4445ad73b1567a5106b87826a0c9a
dlsdk0601 <dlsdk0601@gmail.com>
2024-11-14 01:12:39 +0900

    feat: manager module 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { ComboCardModule } from "./combo-card/combo-card.module";
 import { AssetModule } from "./asset/asset.module";
 import { MainController } from "./main.controller";
+import { ManagerModule } from "./manager/manager.module";
 
 @Module({
-  imports: [ComboCardModule, AssetModule],
+  imports: [ComboCardModule, AssetModule, ManagerModule],
   controllers: [MainController],
   providers: [],
 })
