8caf42ab034b1b1e82c85ba9e5493c890be54eef
inajung <ina.jung@coint.co.kr>
2024-11-14 15:41:09 +0900

    feat: Public 데코레이터 생성 및 미들웨어에서 public 은 통과되게 수정

--- src/middleware/auth.guard.ts
+++ src/middleware/auth.guard.ts
@@ -1,7 +1,16 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  SetMetadata,
+  UnauthorizedException,
+} from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { Request } from "express";
 import { config } from "../config";
+import { CONSTANT } from "../constant";
+import { Reflector } from "@nestjs/core";
+import { API_STATUS } from "./interceptor";
 
 export interface GlobalManager {
   sub: number;
@@ -9,13 +18,38 @@ export interface GlobalManager {
   id: string;
 }
 
+declare global {
+  // namespace 를 되도록 지양하는게 맞으나, global 타입을 위해 여기서만 예외
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Express {
+    interface Request {
+      manager: GlobalManager;
+    }
+  }
+}
+
+export const Public = () => SetMetadata(CONSTANT.IS_PUBLIC_KEY, true);
+
 @Injectable()
 export class AuthGuard implements CanActivate {
   private readonly HEADER_TYPE = "Bearer";
 
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector
+  ) {}
 
   async canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(CONSTANT.IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    // public API 는 인증 제외 시킨다.
+    if (isPublic) {
+      return true;
+    }
+
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
 
